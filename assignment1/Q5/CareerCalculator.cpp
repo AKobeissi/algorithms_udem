ed2771309752a8df14c4b2f7d56837d18500a16b
Amine Kobeissi <102749693+AKobeissi@users.noreply.github.com>
2025-03-07 14:42:04 -0500

    Update CareerCalculator.cpp

--- assignment1/Q5/CareerCalculator.cpp
+++ assignment1/Q5/CareerCalculator.cpp
@@ -14,17 +14,20 @@ CareerCalculator::CareerCalculator()
 
 bool CareerCalculator::CalculateMaxCareer(const vector<int>& Steps) {
     // Completer ici
-    int n = Steps.size();
-    int farthest = 0;
+    int n = Steps.size(); // Total number of career stages
+    int farthest = 0; // Tracks the farthest index that is in reach at any point
 
+    // Loop through each stage in the career.
     for (int i = 0; i < n; i++) {
         if (i > farthest) {
             // If we've reached a point we can't jump to, we fail
             return false;
         }
-        farthest = max(farthest, i + Steps[i]);
+        
+        // Update the farthest reachable stage using the maximum jump from the current stage
+        farthest = std::max(farthest, i + Steps[i]);
         if (farthest >= n - 1) {
-            return true;
+            return true; // If we reach/pass the last stage then our goal is achievable
         }
     }
 
